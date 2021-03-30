7dd425e39c304376f60b319df1dcd2a0bf42c248
김영권 <jake-alden@gim-yeong-gwon-ui-MacBookAir.local>
2021-03-30 16:43:09 +0900

    SignUp Ajax applied

--- UserAccount/urls.py
+++ UserAccount/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('login/', views.page_login, name="login"),
     path('home/', views.index, name="home"),
     path('signup/', views.sign_up, name="sign_up"),
+    path('signup/emailcheck/', views.email_check, name="email_check"),
     path('signup_action/', views.signup_action, name="signup_action"),
     path('login_action/', views.login_action, name="login_action"),
     path('logout_action/', views.logout_action, name="logout_action"),
