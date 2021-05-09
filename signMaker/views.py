0a67c2e6840afa783ced992b1905101cdeb53adc
youngkwon02 <youngkwon02@naver.com>
2021-05-09 20:55:50 +0900

    feat: get sign image at the watermark page

--- signMaker/views.py
+++ signMaker/views.py
@@ -1,3 +1,4 @@
+import shutil
 from django.http.request import HttpRequest
 from django.http.response import HttpResponse, JsonResponse
 from django.shortcuts import redirect, render
@@ -6,9 +7,9 @@ from .models import preservedResult
 import os
 import json
 import sys
-sys.path.insert(1, 'C:/Users/1102k/Desktop/workspace/TheSignature-Web/signMaker/ml/')
+sys.path.insert(
+    1, 'C:/Users/1102k/Desktop/workspace/TheSignature-Web/signMaker/ml/')
 import ml_model
-import shutil
 
 secret_file = os.path.realpath('./secrets.json')
 
@@ -46,12 +47,18 @@ def mainPage(request):
 
 
 def watermarkPage(request):
+    rows = preservedResult.objects.filter(
+        owner_email=request.session['user_email']).values()
+    
     if session_existence(request):
-         return render(request, 'signMaker/watermark.html')
+        if len(rows) > 0:
+            return render(request, 'signMaker/watermark.html', {'path': rows[0]['result_path']})
+        else:
+            return render(request, 'signMaker/watermark.html')
     else:
         return redirect('login')
-    
-    
+
+
 def passOptions(request):
     if not session_existence(request):
         return redirect('login')
@@ -65,22 +72,27 @@ def passOptions(request):
         "pointLetter": request.GET['pointLetter']
     }
     return render(request, 'signMaker/signCreate.html', data)
-    
+
 
 def preserveResult(request):
     if not session_existence(request):
         return redirect('login')
-    rows = preservedResult.objects.filter(owner_email=request.session['user_email'])
+    rows = preservedResult.objects.filter(
+        owner_email=request.session['user_email'])
     path = ""
-    if len(rows) < 5 :
-        path += shutil.copy("./signMaker/static/ml_result/" + request.GET['file_name'], "./signMaker/preservedResult/" + request.session['user_email'] + str(len(rows) + 1) + ".jpg")
-        preservedResult.objects.create(owner_email=request.session['user_email'], owner_last_name_kr=request.session['user_name'], result_path=path)
+    if len(rows) < 5:
+        path += shutil.copy("./signMaker/static/ml_result/" +
+                            request.GET['file_name'], "./signMaker/preservedResult/" + request.session['user_email'] + str(len(rows) + 1) + ".jpg")
+        preservedResult.objects.create(owner_email=request.session['user_email'], owner_last_name_kr=request.session['user_name'],
+                                       result_path="../../signMaker/preservedResult/" + request.session['user_email'] + str(len(rows) + 1) + ".jpg")
     return render(request, 'signMaker/signCreate.html')
 
+
 def is_storable(request):
-    rows = preservedResult.objects.filter(owner_email=request.session['user_email'])
+    rows = preservedResult.objects.filter(
+        owner_email=request.session['user_email'])
     path = ""
-    if len(rows) < 5 :
+    if len(rows) < 5:
         return HttpResponse('True')
     else:
         return HttpResponse('False')
