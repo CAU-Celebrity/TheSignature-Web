2101cbd8ab222ce0b62ef726d19308899a1e9c87
sohyun123 <carriesohyun@gmail.com>
2021-05-17 00:54:34 +0900

    update: change logo font size

--- signMaker/addLogo.py
+++ signMaker/addLogo.py
@@ -6,7 +6,7 @@ def putLogo(imgPath,str_number):
     print(image.shape)
     x = int(image.shape[1]/11)
     y = int(image.shape[0]/5)*3
-    imageWithLogo = cv2.putText(image, "The Signature", (x, y), cv2.FONT_HERSHEY_SCRIPT_COMPLEX, fontScale=1.2, thickness=2,color=(220,220,220))
+    imageWithLogo = cv2.putText(image, "The Signature", (x, y), cv2.FONT_HERSHEY_SCRIPT_COMPLEX, fontScale=0.8, thickness=2,color=(220,220,220))
 
     logoImgDir = './signMaker/static/logo_result/'
     if os.path.isdir(logoImgDir)==False:
